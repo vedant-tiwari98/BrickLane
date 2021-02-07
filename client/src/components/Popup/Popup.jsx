84a755bcf5e77454dc7da02e24a6cd32c83c6c9c
vedanttiwari42 <tiwari.vedant42@gmail.com>
2021-02-07 21:00:29 +0530

    Building Frontend

---
+++ client/src/components/Popup/Popup.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Button, Modal, ModalHeader, ModalFooter } from "reactstrap";
+
+class Popup extends React.Component {
+  state = {
+    modal: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button color="danger" onClick={this.toggle}>
+          <i className="fa fa-trash-o" />
+        </Button>
+        Delete
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle}>
+            {this.props.modelTitle}
+          </ModalHeader>
+          <ModalBody>Are You Sure ?</ModalBody>
+          <ModalFooter>
+            <Button
+              color="danger"
+              onClick={() => this.props.deleteProperty(this.props.id)}
+            >
+              Delete
+            </Button>{" "}
+            <Button color="secondary" onClick={this.toggle}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default Popup;
--- config/keys_devs.js
+++ config/keys_devs.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI: "mongodb://localhost:27017/BrickLane",
   secretOrKey: process.env.SECRET_OR_KEY,
-  GOOGLE_MAP_KEY: process.env.GOOGLE_MAP_KEY,
+  GOOGLE_MAP_KEY: AIzaSyCdAtGLIwEb3cg_0cgL - Ppl5gIEsGp_VVc,
 };
