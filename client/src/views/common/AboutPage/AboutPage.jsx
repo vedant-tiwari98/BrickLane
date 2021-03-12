8da9aa1ff936a4b77b08f9cb2a4c05c1a5ed9e25
vedanttiwari42 <tiwari.vedant42@gmail.com>
2021-03-13 00:34:19 +0530

    adding UI changes

---
+++ client/src/views/common/AboutPage/AboutPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="container">
+      <div className="row my-5">
+        <h2 className="ml-auto mr-auto">About Us</h2>
+        <div className="jumbotron mt-3">
+          <h1 className="display-4">Serving you since 2021</h1>
+          <p className="lead">Real Estate Website</p>
+        </div>
+      </div>
+    </div>
+  );
+};
